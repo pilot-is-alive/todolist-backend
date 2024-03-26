3bf92a112d0dac1ba6a2fee64872d6832608f9c1
pilot-is-alive <lusandanyilenda@gmail.com>
2024-03-26 21:16:06 +0200

    Use express.json() middleware

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ configDotenv({path: '../.env'});
 const app = express();
 const PORT = process.env.PORT || 5000
 
-
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);
