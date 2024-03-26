d91343649b6cdd67e27210440b62c0fb0a78a9a2
pilot-is-alive <lusandanyilenda@gmail.com>
2024-03-26 20:41:57 +0200

    Use process.env variables in PoolConfig

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,10 +1,10 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "to-do-list",
-    password: "Godisalive010204#",
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
     port: 5432
 })
 
